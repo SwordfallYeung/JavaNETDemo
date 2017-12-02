750662d7952862d758ca0b7d17c9cf8c006912cd
qiushangwenyue <2506597416@qq.com>
2017-12-03 02:17:08 +0800

    提交CookieManager,为CookieHandler的子类

---
+++ src/b_abstract_class/CookieManagerDemo.java
@@ -0,0 +1,18 @@
+package b_abstract_class;
+
+/**
+ * @author SwordFall
+ * @create 2017-12-02 22:32.
+ * @desc
+ *
+ *
+ * 父类为CookieHandler
+ * 参考CookieHandler实例
+ *
+ * 方法:
+ * getCookieStore()   获取CookieStore
+ * 还有get和put方法
+ *
+ **/
+public class CookieManagerDemo {
+}
