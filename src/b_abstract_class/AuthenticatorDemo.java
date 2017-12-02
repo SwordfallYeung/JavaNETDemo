8d9acc61568c384e851ee0079a3bfd386ee5a163
GodYeung <2506597416@qq.com>
2017-12-02 14:45:00 +0800

    添加类

--- src/b_abstract_class/AuthenticatorDemo.java
+++ src/b_abstract_class/AuthenticatorDemo.java
@@ -1,4 +1,4 @@
-package b_classes;
+package b_abstract_class;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -14,9 +14,19 @@ import java.net.URL;
  *
  * 抽象类
  *
+ * 这个类Authenticator代表一个对象，通过该对象了解如何验证一个network连接。
+ * 通常，它会在用户信息提示下做这个。
+ *
+ * 应用程序通过在子类覆盖getPasswordAuthentication()方法来使用该类。
+ * 这个方法典型地使用getXXX()方法来获取整个请求权限信息。它必须通过与用户交互或一些其他非交互手段来
+ * 获取用户名和密码。凭证作为PasswordAuthentication返回值返回。
+ *
+ * 产生的子类实例通过调用 setDefault(Authenticator)注册到系统中。当要求身份认证时，系统将调用其中一个requestPasswordAuthentication()方法，
+ * 该方法又会调用注册对象的getPasswordAuthentication()方法。
+ *
+ * 所有请求身份验证的方法都有一个失败的默认实现。
+ *
  *  JAVA提供一个用于启用身份认证的类,可以支持HTTP协议中的多个认证方式,这个类是java.net.Authenticator,使用方法如下:
- *
- *
  *
  **/
 public class AuthenticatorDemo {
---
+++ src/b_abstract_class/CacheRequestDemo.java
@@ -0,0 +1,15 @@
+package b_abstract_class;
+
+/**
+ * @author y15079
+ * @create 2017-11-24 19:48
+ * @desc
+ *
+ * 抽象类
+ *
+ * 在ResponseCache中存储资源的代表通道。这个类的实例提供了OutputStream对象，该对象被protocol handlers调用去在cache存储数据，
+ *
+ *
+ **/
+public class CacheRequestDemo {
+}
