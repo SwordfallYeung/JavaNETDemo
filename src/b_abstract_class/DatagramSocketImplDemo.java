82c86e62de2a5cc258770ab5674cbe23ef15d553
qiushangwenyue <2506597416@qq.com>
2017-12-03 14:48:10 +0800

    提交DatagramSocketImpl,基类

---
+++ src/b_abstract_class/DatagramSocketImplDemo.java
@@ -0,0 +1,16 @@
+package b_abstract_class;
+
+/**
+ * @author SwordFall
+ * @create 2017-12-03 14:44.
+ * @desc
+ *
+ * 抽象类
+ * SocketOptions的实现类
+ *
+ * 抽象数据报和多广播socket实现的基类
+ *
+ **/
+public class DatagramSocketImplDemo {
+
+}
