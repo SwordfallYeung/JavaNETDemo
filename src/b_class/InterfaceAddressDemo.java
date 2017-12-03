3fcb3de188b09c1bb8014f9d8c217f2debd974da
qiushangwenyue <2506597416@qq.com>
2017-12-03 18:41:39 +0800

    提交InterfaceAddress,不知道有什么用,比较NetworkInterface

---
+++ src/b_class/InterfaceAddressDemo.java
@@ -0,0 +1,14 @@
+package b_class;
+
+/**
+ * @author SwordFall
+ * @create 2017-12-03 18:11.
+ * @desc
+ *
+ * 这类代表network接口的地址.简单来说,就是一个IP地址,一个subnet掩码和一个网关地址,当地址是IPv4时.
+ * 一个IP地址和一个network前长度在IPv6地址里面.
+ *
+ * 比较InterfaceAddress和NetworkInterface
+ **/
+public class InterfaceAddressDemo {
+}
