07054a32f281d47d04df40df13df984bfd9a9838
qiushangwenyue <2506597416@qq.com>
2017-12-03 16:34:06 +0800

    提交Inet6Address

---
+++ src/b_class/Inet6AddressDemo.java
@@ -0,0 +1,14 @@
+package b_class;
+
+/**
+ * @author SwordFall
+ * @create 2017-12-03 16:31.
+ * @desc
+ *
+ * final类
+ * 父类为InetAddress
+ *
+ * 参考Inet4Address
+ **/
+public class Inet6AddressDemo {
+}
