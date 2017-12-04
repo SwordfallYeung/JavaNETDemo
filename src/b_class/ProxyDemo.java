f27ea7181e0806727485fab8ec9856affc89f7ff
GodYeung <2506597416@qq.com>
2017-12-04 19:22:42 +0800

    提交Proxy

---
+++ src/b_class/ProxyDemo.java
@@ -0,0 +1,22 @@
+package b_class;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author y15079
+ * @create 2017-12-04 17:04
+ * @desc
+ *
+ * 该类代表一个代理设置，典型的类型（http，socks）和一个socket地址。一个代理是不可变的对象。
+ *
+ *  Proxy(Proxy.Type type ，SocketAddress sa)
+ **/
+public class ProxyDemo {
+
+	public static void main(String[] args) {
+
+	}
+
+}
