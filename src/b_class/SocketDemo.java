4c4e950077016ea2d0f57a98670276a210c2ea39
GodYeung <2506597416@qq.com>
2017-12-11 20:18:05 +0800

    提交Socket

--- src/b_class/SocketDemo.java
+++ src/b_class/SocketDemo.java
@@ -4,6 +4,18 @@ package b_class;
  * @author SwordFall
  * @create 2017-12-05 23:57.
  * @desc
+ *
+ * 1.TCP协议是面向连接的、可靠的、有序的、以字节流的方式发送数据，通过三次握手方式建立连接，形成传输数据的通道，在连接中进行大量数据的传输，效率会稍低
+ * 2.Java中基于TCP协议实现网络通信的类
+ *   客户端的Socket类
+     服务器端的ServerSocket类
+ *
+ * 3.Socket通信的步骤
+ *    ①创建ServerSocket和Socket
+ *
+ *
+ * https://www.cnblogs.com/rocomp/p/4790340.html
  **/
 public class SocketDemo {
+
 }
