90d681b3a7179fddab072f3494ab41277abafb2a
qiushangwenyue <2506597416@qq.com>
2017-12-22 00:53:43 +0800

    提交URLStreamHandler

---
+++ src/b_class/URLStreamHandlerDemo.java
@@ -0,0 +1,17 @@
+package b_class;
+
+/**
+ * @author SwordFall
+ * @create 2017-12-22 00:51.
+ * @desc
+ *
+ * URL协议扩展实现
+ *
+ * java.net.URLStreamHandler 是一个工厂类，通过 openConnection(java.net.URL) 方法来创建 java.net.URLConnection的实例。 java.net.URLStreamHandler 实现灵活度很大，既可以通过不同 protocol 的 URL 实例，产生java.net.URLConnection 对象。还可以通过相同 protocol 的多个 URL 对象，来产生对象。通用性实现，一种协议对应一个java.net.URLStreamHandler 实现。比如，在 SUN JDK 中 sun.net.www.protocol 子包下面的多个 Handler 类就是很好的例子
+ *
+ *
+ *
+ * http://blog.csdn.net/xiaomin1991222/article/details/50980754
+ **/
+public class URLStreamHandlerDemo {
+}
